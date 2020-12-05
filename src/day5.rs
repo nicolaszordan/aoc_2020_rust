00b566bdc97e075c3fcd06fb20ff6948d5eee37c
Nicolas Zordan <nicolas.zordan.tek@gmail.com>
2020-12-05 13:06:27 +0100

    day5 part1

---
+++ src/day5.rs
@@ -0,0 +1,54 @@
+use std::fs::File;
+use std::io::Read;
+
+fn solve_part1(input: &str) -> u32 {
+    input
+        .lines()
+        .map(|line| {
+            line.chars()
+                .take(7)
+                .map(|c| match c {
+                    'F' => 0,
+                    'B' => 1,
+                    _ => panic!(),
+                })
+                .fold(0, |acc, bit| acc << 1 | bit)
+                * 8
+                + line
+                    .chars()
+                    .skip(7)
+                    .map(|c| match c {
+                        'L' => 0,
+                        'R' => 1,
+                        _ => panic!(),
+                    })
+                    .fold(0, |acc, bit| acc << 1 | bit)
+        })
+        .max()
+        .unwrap()
+}
+
+pub fn part1() {
+    let mut file = File::open("input/2020/day5.txt").unwrap();
+    let mut input = String::new();
+    file.read_to_string(&mut input).unwrap();
+    println!("{}", solve_part1(&input));
+}
+
+mod test {
+    use super::*;
+
+    #[test]
+    fn solve_part1_example() {
+        assert_eq!(solve_part1("FBFBBFFRLR"), 357);
+    }
+
+    #[test]
+    fn solve_part1_test() {
+        assert_eq!(solve_part1("FFFFFFFLLL"), 0);
+        assert_eq!(solve_part1("FFFFFFBLLL"), 8);
+        assert_eq!(solve_part1("FFFFFFFLLR"), 1);
+        assert_eq!(solve_part1("FFFFFFFLRL"), 2);
+        assert_eq!(solve_part1("FFFFFFFRLR"), 5);
+    }
+}
